2a5cf4331752f27a07d5c4b8dc7b3134383218ca
Trọng Phạm <phamtrong1920@gmail.com>
2026-09-16 14:03:24 +0700

    add

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       max_memory_restart: "256M",
       env: {
         NODE_ENV: "production",
+        PORT: 8001,
       },
       env_file: ".env",
     },
--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const routes = require('./server/routes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
